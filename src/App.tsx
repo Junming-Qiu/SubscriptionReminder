337b646af491465b2104183e110a246613ee3932
Junming Qiu <junming@Junming.fios-router.home>
2022-01-15 14:20:03 -0500

    routing added/tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './pages/main';
+import History from './pages/history'
+import Settings from './pages/settings';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
+
+//Pure routing
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
-          <Route path="/about" element={<About/>}>
+          <Route path="/settings" element={<Settings/>}>
    
           </Route>
-          <Route path="/users" element={<Users />}>
+          <Route path="/history" element={<History />}>
         
           </Route>
-          <Route path="/" element={<Home />}>
+          <Route path="/" element={<Main />}>
       
           </Route>
         </Routes>
-      </div>
     </Router>
-    
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ src/pages/settings.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+function Settings(props){
+    return (
+        <div>
+            <h2>Settings</h2>
+            <button type="button" class="btn btn-outline-primary">Primary</button>
+        </div>
+        
+    )
+}
+
+export default Settings;
