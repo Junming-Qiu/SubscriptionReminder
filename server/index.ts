aab57ed453625293802ef8b6266a08e6cff37643
Kevin Wu <sxw207@nyu.edu>
2022-01-28 00:06:06 -0500

    set up with gql and express

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,62 @@
-// import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import { graphqlHTTP } from 'express-graphql';
+import { createConnection } from 'typeorm';
 
 
-// const express = require('express');
-import express from 'express';
-import { Request, Response } from 'express'
+const main = async () => {
 
-const app = express();
-const port = 3001;
+  // await createConnection({
+  //   type: "mysql",
+  //   database: "", //Database Name
+  //   username: "",
+  //   password: "",
+  //   logging: true,
+  //   synchronize: false, //determine if make tables automatically or not 
+  //   entities: [] //entities for
+  // })
 
-app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
-});
+  const app = express();
+  const port = 3001;
 
-// import path from "path";
+  //Cors middleware allows for frontend to recieve information from url other than its own
+  app.use(cors({origin: "http://localhost:3000/"}))
+  //parse all incoming JSON payloads into objects
+  app.use(express.json())
+
+  //Apply graphql middleware
+  // app.use("/graphql", graphqlHTTP({
+  //   schema,
+  //   graphiql: true
+  // }))
+
+  //start server on port 
+  app.listen(port, () => {
+    console.log(`SERVER RUNNING ON PORT ${port}`);
+  });
+
+
+
+  app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World')
+  })
+
+}
+
+//run and catch if error
+main().catch((err) => {
+  console.log(err);
+})
+
+
+
+
+
+
+
+
 
 // const port = 3002;
 
 // const app = express()
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World')
-})
-
-// app.listen(port)
